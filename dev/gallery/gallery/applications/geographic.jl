f1c82cd3510f2cb0866ea47eae4bb4fffb97f763
Documenter.jl <documenter@juliadocs.github.io>
2023-01-26 13:46:12 +0000

    build based on 00c3d05

--- dev/gallery/gallery/applications/geographic.jl
+++ dev/gallery/gallery/applications/geographic.jl
@@ -1,11 +1,11 @@
 using AlgebraOfGraphics, CairoMakie
 using Shapefile, ZipFile
-using Downloads: download
+using Downloads
 
 # Download, extract, and load shapefile
 t = mktempdir() do dir
-    url = "https://data.bas.ac.uk/download/7be3ab29-7caa-46b8-a355-2e3233796e86"
-    r = ZipFile.Reader(seekstart(download(url, IOBuffer())))
+    url = "https://ramadda.data.bas.ac.uk/repository/entry/get/add_coastline_medium_res_polygon_v7_4.zip?entryid=synth%3Ae747e63e-9d93-49c2-bafc-cf3d3f8e5afa%3AL2FkZF9jb2FzdGxpbmVfbWVkaXVtX3Jlc19wb2x5Z29uX3Y3XzQuemlw"
+    r = ZipFile.Reader(seekstart(Downloads.download(url, IOBuffer())))
     for f in r.files
         open(joinpath(dir, f.name), write = true) do io
             write(io, read(f, String));
